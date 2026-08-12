c5ba7c73c5934facb31d25331b8ace2c8e8ff79f
karsakovilya51-maker <karsakovilya51@gmail.com>
2026-08-12 09:34:10 +0300

    config.py

--- config.py
+++ config.py
@@ -8,5 +8,8 @@ CHANNEL_ID = os.getenv("CHANNEL_ID")
 CHANNEL_URL = os.getenv("CHANNEL_URL")
 ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
 
+# Ссылка на рабочий чат для перенаправления лидов
+SUPPORT_CHAT_URL = os.getenv("SUPPORT_CHAT_URL", "https://t.me/+JlpYTznXUw01OWQy")
+
 if not BOT_TOKEN or not CHANNEL_ID or not CHANNEL_URL:
-    raise ValueError("Ошибка: Не заполнена одна из обязательных переменных окружения!")
+    raise ValueError("Ошибка: Не заполнены обязательные переменные окружения!")
